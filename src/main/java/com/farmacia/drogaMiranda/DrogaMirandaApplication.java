c39e4be22e546bfc7d3735e09d24cb9a7a7a3bda
Debora <debvm@hotmail.com>
2021-02-23 14:32:52 -0300

    Backend Drogaria Miranda - Exercício Generation

---
+++ src/main/java/com/farmacia/drogaMiranda/DrogaMirandaApplication.java
@@ -0,0 +1,13 @@
+package com.farmacia.drogaMiranda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrogaMirandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrogaMirandaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/farmacia/drogaMiranda/DrogaMirandaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmacia.drogaMiranda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrogaMirandaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
